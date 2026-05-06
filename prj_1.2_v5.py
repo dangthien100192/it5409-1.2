74d75e3364ba120424262ffee25846ecefb0a324
thiendd8 <thiendd8@viettel.com.vn>
2026-05-06 17:17:59 +0700

    Bổ sung xử lý ảnh tối/nhiễu

--- prj_1.2_v5.py
+++ prj_1.2_v5.py
@@ -174,7 +174,118 @@ def split_touching_rice_local(roi_bgr, roi_mask, min_area=18):
 
     return split_contours
 
+def adjust_gamma(image, gamma=0.75, brightness=20):
+    """
+    gamma < 1  -> tăng sáng vùng tối
+    brightness -> cộng sáng toàn ảnh
+    """
+    invGamma = 1.0 / gamma
 
+    table = np.array([
+        ((i / 255.0) ** invGamma) * 255
+        for i in np.arange(256)
+    ]).astype("uint8")
+
+    gamma_corrected = cv2.LUT(image, table)
+
+    # tăng sáng thêm
+    bright = cv2.convertScaleAbs(
+        gamma_corrected,
+        alpha=1.0,
+        beta=brightness
+    )
+
+    return bright
+
+def remove_uneven_wave_noise(gray, ksize=51):
+    """
+    Lọc nhiễu nền không đồng đều / nhiễu sóng tần số thấp.
+    ksize càng lớn thì lọc nền càng mạnh.
+    Nên dùng số lẻ: 31, 51, 71.
+    """
+    background = cv2.GaussianBlur(gray, (ksize, ksize), 0)
+
+    corrected = cv2.subtract(gray, background)
+    corrected = cv2.normalize(
+        corrected, None, 0, 255, cv2.NORM_MINMAX
+    ).astype(np.uint8)
+
+    return corrected
+
+def has_column_wave_noise(gray, smooth=31, threshold=8.0):
+    """
+    Kiểm tra ảnh có nhiễu/vân theo cột hay không
+    dựa trên profile sóng cột.
+    """
+    gray_f = gray.astype(np.float32)
+
+    # Profile sáng theo cột
+    col_profile = np.mean(gray_f, axis=0)
+
+    # Làm mượt để lấy sóng lớn
+    col_profile = cv2.GaussianBlur(
+        col_profile.reshape(1, -1),
+        (smooth, 1),
+        0
+    ).reshape(-1)
+
+    # Thành phần sóng
+    col_wave = col_profile - np.mean(col_profile)
+
+    # Độ mạnh sóng
+    wave_strength = np.std(col_wave)
+
+    return wave_strength > threshold, wave_strength
+def remove_column_wave_noise(gray, strength=1.1, smooth=31):
+    """
+    Khử vân/sóng dọc theo cột.
+    Phù hợp ảnh có sọc sáng tối chạy từ trên xuống.
+    """
+    gray_f = gray.astype(np.float32)
+
+    # Tính profile sáng tối theo từng cột
+    col_profile = np.mean(gray_f, axis=0)
+
+    # Làm mượt profile để lấy sóng lớn, bỏ chi tiết hạt nhỏ
+    col_profile = cv2.GaussianBlur(
+        col_profile.reshape(1, -1),
+        (smooth, 1),
+        0
+    ).reshape(-1)
+
+    # Đưa profile về quanh 0
+    col_wave = col_profile - np.mean(col_profile)
+
+    # Trừ sóng khỏi từng cột
+    corrected = gray_f - strength * col_wave.reshape(1, -1)
+
+    corrected = np.clip(corrected, 0, 255).astype(np.uint8)
+    return corrected
+
+def need_adjust_gamma(
+    gray,
+    dark_threshold=25,
+    low_contrast_threshold=10
+):
+    """
+    Kiểm tra ảnh có cần tăng sáng/gamma hay không.
+    """
+
+    # Độ sáng trung bình
+    mean_intensity = np.mean(gray)
+
+    # Độ tương phản
+    contrast = np.std(gray)
+
+    # Ảnh tối
+    is_dark = mean_intensity < dark_threshold
+
+    # Ảnh bị lì / tương phản thấp
+    low_contrast = contrast < low_contrast_threshold
+
+    need_gamma = is_dark or low_contrast
+
+    return need_gamma, mean_intensity, contrast
 def count_rice_grains(image_path, output_folder=None, save_steps=False, show_steps=False):
     img = cv2.imread(image_path)
     if img is None:
@@ -184,14 +295,29 @@ def count_rice_grains(image_path, output_folder=None, save_steps=False, show_ste
 
     original = img.copy()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
+    # Lọc nhiễu nền không đồng đều / dạng sóng
+    has_column_wave, wave_strength = has_column_wave_noise(gray)
+    if(has_column_wave):
+        print(f"Ảnh nhiễu sọc, độ nhiễu: {wave_strength}")
+        gray_denoise = remove_column_wave_noise(gray)
+    else:
+        gray_denoise = gray.copy()
+    need_gamma, mean_intensity, contrast = need_adjust_gamma(gray_denoise)
+    if(need_gamma):
+        print("Ảnh tối, cần làm sáng")
+        gray_bright = adjust_gamma(gray_denoise, gamma=3)
+    else:
+        gray_bright = gray_denoise.copy()
     if save_steps and output_folder:
         save_step_image(output_folder, original_filename, 0, "original", original)
         save_step_image(output_folder, original_filename, 1, "gray", gray)
+        if has_column_wave:
+            save_step_image(output_folder, original_filename, 1, "gray_denoise", gray_denoise)
+        save_step_image(output_folder, original_filename, 2, "gray_bright", gray_bright)
 
-    # Tăng tương phản cục bộ
+    # Gtăng tương phản nhẹ
     clahe = cv2.createCLAHE(clipLimit=1.8, tileGridSize=(8, 8))
-    gray_clahe = clahe.apply(gray)
+    gray_clahe = clahe.apply(gray_bright)
 
     # Blur nhẹ để không mất hạt nhỏ
     blur = cv2.GaussianBlur(gray_clahe, (5, 5), 0)
@@ -207,7 +333,7 @@ def count_rice_grains(image_path, output_folder=None, save_steps=False, show_ste
     )
 
     white_ratio = np.sum(thresh == 255) / thresh.size
-    if white_ratio > 0.5:
+    if white_ratio > 0.70:
         _, thresh = cv2.threshold(
             blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU
         )
@@ -218,23 +344,25 @@ def count_rice_grains(image_path, output_folder=None, save_steps=False, show_ste
     # Morphology nhẹ để tránh sót
     kernel = np.ones((3, 3), np.uint8)
     opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=1)
+    closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel, iterations=1)
 
     if save_steps and output_folder:
-        save_step_image(output_folder, original_filename, 5, "morphologyEx", opening)
+        save_step_image(output_folder, original_filename, 5, "opening", opening)
+        save_step_image(output_folder, original_filename, 6, "closing", closing)
 
     # Sure BG
-    sure_bg = cv2.dilate(opening, kernel, iterations=2)
+    sure_bg = cv2.dilate(closing, kernel, iterations=2)
 
     if save_steps and output_folder:
-        save_step_image(output_folder, original_filename, 6, "sure_bg", sure_bg)
+        save_step_image(output_folder, original_filename, 7, "sure_bg", sure_bg)
 
     # Distance transform toàn ảnh
-    dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
+    dist_transform = cv2.distanceTransform(closing, cv2.DIST_L2, 5)
     dist_transform_norm = cv2.normalize(
         dist_transform, None, 0, 255, cv2.NORM_MINMAX
     ).astype(np.uint8)
 
-    # Sure FG
+    # Threshold mềm để đỡ sót hạt
     _, sure_fg = cv2.threshold(
         dist_transform, 0.28 * dist_transform.max(), 255, 0
     )
@@ -243,9 +371,9 @@ def count_rice_grains(image_path, output_folder=None, save_steps=False, show_ste
     unknown = cv2.subtract(sure_bg, sure_fg)
 
     if save_steps and output_folder:
-        save_step_image(output_folder, original_filename, 7, "distance_transform", dist_transform_norm)
-        save_step_image(output_folder, original_filename, 8, "sure_fg", sure_fg)
-        save_step_image(output_folder, original_filename, 9, "unknown", unknown)
+        save_step_image(output_folder, original_filename, 8, "distance_transform", dist_transform_norm)
+        save_step_image(output_folder, original_filename, 9, "sure_fg", sure_fg)
+        save_step_image(output_folder, original_filename, 10, "unknown", unknown)
 
     # Marker
     num_markers, markers = cv2.connectedComponents(sure_fg)
@@ -256,7 +384,7 @@ def count_rice_grains(image_path, output_folder=None, save_steps=False, show_ste
         markers_vis_before = cv2.normalize(
             markers.astype(np.float32), None, 0, 255, cv2.NORM_MINMAX
         ).astype(np.uint8)
-        save_step_image(output_folder, original_filename, 10, "markers_before_watershed", markers_vis_before)
+        save_step_image(output_folder, original_filename, 11, "markers_before_watershed", markers_vis_before)
 
     # Watershed toàn ảnh
     markers = cv2.watershed(original, markers)
@@ -266,7 +394,7 @@ def count_rice_grains(image_path, output_folder=None, save_steps=False, show_ste
         markers_vis_after = cv2.normalize(
             markers_vis_after, None, 0, 255, cv2.NORM_MINMAX
         ).astype(np.uint8)
-        save_step_image(output_folder, original_filename, 11, "markers_after_watershed", markers_vis_after)
+        save_step_image(output_folder, original_filename, 12, "markers_after_watershed", markers_vis_after)
 
     output = original.copy()
     valid_mask = np.zeros(gray.shape, dtype=np.uint8)
@@ -280,7 +408,7 @@ def count_rice_grains(image_path, output_folder=None, save_steps=False, show_ste
     min_area = 18
     max_area = 7000
 
-    # Lấy các contour
+    # Lấy các contour như bản đang chạy tốt
     for marker_id in unique_markers:
         if marker_id <= 1:
             continue
@@ -451,9 +579,9 @@ def count_rice_grains(image_path, output_folder=None, save_steps=False, show_ste
     )
 
     if save_steps and output_folder:
-        save_step_image(output_folder, original_filename, 12, "valid_mask", valid_mask)
-        save_step_image(output_folder, original_filename, 13, "valid_overlay", valid_overlay)
-        save_step_image(output_folder, original_filename, 14, "final_output", output)
+        save_step_image(output_folder, original_filename, 13, "valid_mask", valid_mask)
+        save_step_image(output_folder, original_filename, 14, "valid_overlay", valid_overlay)
+        save_step_image(output_folder, original_filename, 15, "final_output", output)
 
     if show_steps:
         cv2.imshow("Original", original)
@@ -474,26 +602,6 @@ def count_rice_grains(image_path, output_folder=None, save_steps=False, show_ste
 
     return rice_count, output
 
-def print_info(image_path):
-    img = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
-
-    # Kích thước ảnh
-    height, width = img.shape[:2]
-
-    # Số kênh màu
-    if len(img.shape) == 2:
-        channels = 1
-    else:
-        channels = img.shape[2]
-
-    # Bit depth
-    bit_depth = img.dtype.itemsize * 8
-
-    print("Width:", width)
-    print("Height:", height)
-    print("Size:", f"{width}x{height}")
-    print("Channels:", channels)
-    print("Bit depth:", bit_depth)
 
 def process_folder(input_folder, output_folder="output", save_steps=True):
     os.makedirs(output_folder, exist_ok=True)
@@ -507,7 +615,13 @@ def process_folder(input_folder, output_folder="output", save_steps=True):
 
     for image_path in image_files:
         try:
-            print_info(image_path)
+            img = cv2.imread(image_path)
+            h, w = img.shape[:2]
+            print("Processing: ", image_path)
+            print("Height:", h)
+            print("Width:", w)
+            print(img.shape)
+            print(img.dtype)
             count, output_img = count_rice_grains(
                 image_path,
                 output_folder=output_folder,
